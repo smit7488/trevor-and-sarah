02a84ac9f8e549eab9888500b559c733b24fa46c
Trevor Smith <50749735+smit7488@users.noreply.github.com>
2025-09-22 15:38:45 +0000

    Removed usestate

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,17 +1,15 @@
-// src/App.tsx
-
-import { useState } from "react";
 import tsLogo from "./assets/trevor-and-sarah-logo-vertical.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
-
   return (
     <>
       <div>
-          <img src={tsLogo} className="logo trevorandsarah" alt="Trevor & Sarah logo" />
+        <img
+          src={tsLogo}
+          className="logo trevorandsarah"
+          alt="Trevor & Sarah logo"
+        />
       </div>
       <h1>Coming Soon!</h1>
     </>
